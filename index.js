53acf94176f743cbeab9ce4f58308dfe8abbfb96
AleixMJ <aleixtechIT@gmail.com>
2026-03-04 11:11:32 +0100

    completed the Book constructor function and the addBookToLibrary

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+
+    let readYet = read.toLowerCase();
+    if (read != "yes" && read != "no") {
+        throw Error("the parameter read can only be 'yes' or no")
+    }
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = readYet;
+  this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+    
 }
