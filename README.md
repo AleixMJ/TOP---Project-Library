980b4c75e5ec5dae05a266f2d815aaa641a8506f
AleixMJ <aleixtechIT@gmail.com>
2026-04-12 13:04:06 +0200

    added prettier package

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TOP---Project-Library
+
 https://www.theodinproject.com/lessons/node-path-javascript-library
 
 Refactoring the code now with Classes
--- index.js
+++ index.js
@@ -11,17 +11,14 @@ const LibraryTab = document.querySelector("#LibraryTab");
 
 /* Displaying the books on the Library */
 
-
 LibraryTab.addEventListener("click", () => createLibrary());
-
 
 /* Adding book form display and processing process */
 
-
 addBookTab.addEventListener("click", () => {
-    if (formVisible) return;    
-    form.className = "book-form";
-    form.innerHTML = `
+  if (formVisible) return;
+  form.className = "book-form";
+  form.innerHTML = `
         <h2>ADD NEW BOOK</h2>
         
         <div class="form-group">
@@ -53,135 +50,119 @@ addBookTab.addEventListener("click", () => {
             <button type="submit">Add Book</button>
         </div>
     `;
-    libraryDisplay.innerHTML = "";
-    libraryDisplay.hidden = true;
-    content.appendChild(form);
-    formVisible = true;
-    form.addEventListener('submit', handleSubmit);
-
-
-    
+  libraryDisplay.innerHTML = "";
+  libraryDisplay.hidden = true;
+  content.appendChild(form);
+  formVisible = true;
+  form.addEventListener("submit", handleSubmit);
 });
 
-
 function createLibrary() {
-    console.log("creating Library");
-    if (formVisible == true) {
-        form.reset();
-        form.remove();
-        formVisible = false;
-    }
-    libraryDisplay.innerHTML = "";
-    libraryDisplay.hidden = false;
-    const BookCardTitles = document.createElement("div");
-    BookCardTitles.className = "book-card";
-    BookCardTitles.innerHTML = ` <div> Title </div>
+  console.log("creating Library");
+  if (formVisible == true) {
+    form.reset();
+    form.remove();
+    formVisible = false;
+  }
+  libraryDisplay.innerHTML = "";
+  libraryDisplay.hidden = false;
+  const BookCardTitles = document.createElement("div");
+  BookCardTitles.className = "book-card";
+  BookCardTitles.innerHTML = ` <div> Title </div>
     <div> Author </div>
     <div> Pages </div>
     <div> Read </div>
     <div> Delete </div>
-    `
-    libraryDisplay.appendChild(BookCardTitles);
-    for (let book of myLibrary) {
-        const bookCard = document.createElement("div");
-        bookCard.className = "book-card";
-        bookCard.innerHTML = `<div class="book-title">${book.title}</div>
+    `;
+  libraryDisplay.appendChild(BookCardTitles);
+  for (let book of myLibrary) {
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookCard.innerHTML = `<div class="book-title">${book.title}</div>
         <div class="book-author">${book.author}</div>
         <div class="book-pages">${book.pages} pages</div>
         <div class="book-read"> <button class="read-button" data-id="${book.id}"
          data-read="${book.read}"></button> </div>
         <button class="delete-button" data-id="${book.id}"></button>
         `;
-        const readBtn = bookCard.querySelector(".read-button");
-        readBtn.addEventListener("click", () => {
-            book.toggleRead();
-            createLibrary();
-        })
-        const deleteBtn = bookCard.querySelector(".delete-button");
-        deleteBtn.addEventListener("click", () => {
-        const bookId = deleteBtn.dataset.id;
-        myLibrary = myLibrary.filter(book => book.id != bookId);
-        createLibrary();  
-        })
-        libraryDisplay.appendChild(bookCard);
-    }
-
+    const readBtn = bookCard.querySelector(".read-button");
+    readBtn.addEventListener("click", () => {
+      book.toggleRead();
+      createLibrary();
+    });
+    const deleteBtn = bookCard.querySelector(".delete-button");
+    deleteBtn.addEventListener("click", () => {
+      const bookId = deleteBtn.dataset.id;
+      myLibrary = myLibrary.filter((book) => book.id != bookId);
+      createLibrary();
+    });
+    libraryDisplay.appendChild(bookCard);
+  }
 }
 
-
-
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // check custom validity
-    const authorInput = form.querySelector("#author");
-    const titleInput = form.querySelector("#title");
-    const pagesInput = form.querySelector("#pages");
+  // check custom validity
+  const authorInput = form.querySelector("#author");
+  const titleInput = form.querySelector("#title");
+  const pagesInput = form.querySelector("#pages");
 
-    authorInput.setCustomValidity("");
-    titleInput.setCustomValidity("");
-    pagesInput.setCustomValidity("");
-    let isValid = true;
+  authorInput.setCustomValidity("");
+  titleInput.setCustomValidity("");
+  pagesInput.setCustomValidity("");
+  let isValid = true;
 
-    if (authorInput.value.trim() === "") {
-        authorInput.setCustomValidity(`The author name must be filled!`);
-        isValid = false;
-        
+  if (authorInput.value.trim() === "") {
+    authorInput.setCustomValidity(`The author name must be filled!`);
+    isValid = false;
+  }
 
-    }
+  if (titleInput.value.trim() === "") {
+    titleInput.setCustomValidity(`The title must be filled!`);
+    isValid = false;
+  }
 
-        if (titleInput.value.trim() === "") {
-        titleInput.setCustomValidity(`The title must be filled!`);
-        isValid = false;     
+  if (pagesInput.value.trim() === "") {
+    pagesInput.setCustomValidity(`The number of pages must be filled!`);
+    isValid = false;
+  }
 
-    }
+  if (!isValid) {
+    form.reportValidity();
+    return;
+  }
 
-    if (pagesInput.value.trim() === "") {
-        pagesInput.setCustomValidity(`The number of pages must be filled!`);
-        isValid = false;
-    }
-
-    if (!isValid) {
-        form.reportValidity();
-        return;
-    }
-
-    // add book
-    const submittedform = event.target;
-    const formData = new FormData(submittedform);
-    const data = Object.fromEntries(formData);
-    addBookToLibrary(data.title, data.author, data.pages, data.read);
-    submittedform.reset();
-    submittedform.remove();
-    formVisible = false;
-    alert("Book added to the Library");
-
+  // add book
+  const submittedform = event.target;
+  const formData = new FormData(submittedform);
+  const data = Object.fromEntries(formData);
+  addBookToLibrary(data.title, data.author, data.pages, data.read);
+  submittedform.reset();
+  submittedform.remove();
+  formVisible = false;
+  alert("Book added to the Library");
 }
 
 class Book {
-
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        let readYet = read.toLowerCase();
-        if (read != "yes" && read != "no") {
-            throw Error("the parameter read can only be 'yes' or no")
-        }
-        this.read = readYet;
-        this.id = crypto.randomUUID();
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    let readYet = read.toLowerCase();
+    if (read != "yes" && read != "no") {
+      throw Error("the parameter read can only be 'yes' or no");
     }
-    
+    this.read = readYet;
+    this.id = crypto.randomUUID();
+  }
 
-        
-    toggleRead() {
-        this.read = (this.read == "yes") ? "no" : "yes";
-    }
+  toggleRead() {
+    this.read = this.read == "yes" ? "no" : "yes";
+  }
 }
-
 
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-    
 }
